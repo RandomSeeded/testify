023c2cbfc070b20692de9b34491406bdfaed06a0
Nate Willard <nate.a.willard@gmail.com>
2015-09-28 17:30:38 -0700

    Add tests for having the necessary methods

--- test/unit/utilities.js
+++ test/unit/utilities.js
@@ -4,8 +4,10 @@ var expect = chai.expect;
 
 describe('the todo.App', function() {
   describe('the todo object', function(){
-
     it('should have all the necessary methods', function(){
+      todo.util.trimTodoName.should.be.a('function');
+      expect(todo.util.getUniqueId).to.be.a('function');
+      assert.typeOf(todo.util.isValidTodoName, 'function');
     });
   });
 });
