723b7b2eddaa5e6c8e9173d232e7e3d8e9081907
Nate Willard <nate.a.willard@gmail.com>
2015-10-02 18:25:58 -0700

    Progress on casper testing

--- test/e2e/todoFunctionality.js
+++ test/e2e/todoFunctionality.js
@@ -4,11 +4,14 @@ casper.test.begin('App is setup correctly', 2, function suite(test) {
     test.assertExists('.todo-list', 'List should exist');
     test.assertExists('.todo-form', 'Form should exist');
     this.fill('form.todo-form', {
-      todo: 'asdf'
+      todo: 'new todo'
     }, true);
   });
   
   casper.then(function() {
+    test.assertEval(function() {
+      return __utils__.findAll('.todo-item').length >= 2;
+    });
   });
 
   casper.run(function() {
